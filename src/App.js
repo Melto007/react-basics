6b465627c612737418fcf1fe48e25d853fa50557
melto.sm <meltosm8@gmail.com>
2023-02-19 13:48:00 -0800

    learned useState hook

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
 
 function App() {
+  const [ counter, setCounter ] = useState(0)
+  const [ title, setTitle ] = useState('')
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+        <h1>Counter: {counter}</h1>
+        <button onClick={() => setCounter(counter + 1)}>Increase</button>
+
+        <input onChange={(e) => setTitle(e.target.value)} />
+        <p>{title}</p>
+    </React.Fragment>
   );
 }
 
