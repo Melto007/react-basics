286ecb142ea520b09ad4ff21c083ee7cee38ecd8
melto.sm <meltosm8@gmail.com>
2023-02-19 15:43:00 -0800

    learned useReducer hook

--- src/App.js
+++ src/App.js
@@ -1,16 +1,46 @@
-import React, { useState } from 'react'
+import React, { useState, useReducer } from 'react'
+
+const reducer = (state, action) => {
+  switch(action.type) {
+    case "INCREMENT":
+      return {
+        count: state.count + 1
+      }
+    case "DECREMENT": 
+      return {
+        count: state.count - 1
+      }
+    case "RESET":
+      return {
+        count: 0
+      }
+    default:
+      return state
+  }
+}
 
 function App() {
   const [ counter, setCounter ] = useState(0)
   const [ title, setTitle ] = useState('')
 
+  const [ state, dispatch ] = useReducer(reducer, { count: 0 })
+
   return (
     <React.Fragment>
-        <h1>Counter: {counter}</h1>
-        <button onClick={() => setCounter(counter + 1)}>Increase</button>
+        <div>
+          <h1>Counter: {counter}</h1>
+          <button onClick={() => setCounter(counter + 1)}>Increase</button>
 
-        <input onChange={(e) => setTitle(e.target.value)} />
-        <p>{title}</p>
+          <input onChange={(e) => setTitle(e.target.value)} />
+          <p>{title}</p>
+        </div>
+
+        <div>
+          <h1>Count: {state.count}</h1>
+          <button onClick={() => dispatch({ type: "INCREMENT" })}>+</button>
+          <button onClick={() => dispatch({ type: "DECREMENT" })}>-</button>
+          <button onClick={() => dispatch({ type: "RESET" })}>reset</button>
+        </div>
     </React.Fragment>
   );
 }
